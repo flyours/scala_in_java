e209a9ede639a7158c0c47f074ffa5660cdce642
zhangsj <sjzhang@thoughtworks.com>
2016-03-28 10:57:10 +0800

    refactor dir

---
+++ src/main/scala/com/jeff/scala/ScalaJavaMash.scala
@@ -0,0 +1,40 @@
+package com.jeff.scala
+
+import org.slf4j.LoggerFactory
+
+trait Persistable[T] {
+  def log(m: String) = {
+    new DualLogger().log(m)
+  }
+
+  def getEntity: T
+
+  def save(): T = {
+    persistToDb(getEntity)
+    log("save done")
+    getEntity
+  }
+
+  private def persistToDb(t: T) = {}
+}
+
+
+trait RemoteLogger extends java.rmi.Remote {
+  @throws(classOf[java.rmi.RemoteException])
+  def log(m: String)
+}
+
+class PersistableImpl extends Persistable[PersistableImpl] {
+  override def getEntity = {
+    this
+  }
+}
+
+object Main {
+  val logger = LoggerFactory.getLogger(Main.getClass)
+
+  def main(args: Array[String]): Unit = {
+    new PersistableImpl().save()
+    logger.debug("save done")
+  }
+}
---
+++ src/main/scala/com/jeff/scala/ScalaUtils.scala
@@ -0,0 +1,8 @@
+package com.jeff.scala
+
+
+object ScalaUtils {
+  def log(msg: String): Unit = println(msg)
+
+  val MAX_LOG_SIZE = 1056
+}
