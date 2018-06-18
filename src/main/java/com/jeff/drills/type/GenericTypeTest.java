23f586f39f00a84aaa72e69131f9c6e0994dc352
zhangshengjie <zhangshengjie02@meituan.com>
2018-06-18 23:30:32 +0800

    add type variable

--- src/main/java/com/jeff/drills/type/GenericTypeTest.java
+++ src/main/java/com/jeff/drills/type/GenericTypeTest.java
@@ -3,27 +3,42 @@ package com.jeff.drills.type;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class TypeVariableTest<K extends Item & Test2, V>{
-    private static final Logger LOGGER=LoggerFactory.getLogger(TypeVariableTest.class);
+public class GenericTypeTest<K extends Item & Test2, V> extends STestInterface<Item>  {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GenericTypeTest.class);
     K key;
     V value;
-    public static void main(String[] args) throws Exception
-    {
-        TypeVariable[] types = TypeVariableTest.class.getTypeParameters();
-        for(TypeVariable t : types){
-            LOGGER.info("getGenericDeclaration: {}",t.getGenericDeclaration());
+
+    STestInterface<K>[][][] keys;
+    public static void main(String[] args) throws Exception {
+        //ParameterizedType
+        Type genericSuperclass = GenericTypeTest.class.getGenericSuperclass();
+        LOGGER.info("genericSuperclass: {}, is ParameterizedType: {}",genericSuperclass, genericSuperclass instanceof ParameterizedType);
+
+        //TypeVariable
+        TypeVariable[] types = GenericTypeTest.class.getTypeParameters();
+        for (TypeVariable t : types) {
+            LOGGER.info("getGenericDeclaration: {}", t.getGenericDeclaration());
             LOGGER.info("typevariable,name:{},bounds:{}",
                     t.getName(),
                     Arrays.stream(t.getBounds()).map(Object::toString).collect(Collectors.toList()));
         }
+
+        //GenericArrayType
+        Type type = GenericTypeTest.class.getDeclaredField("keys").getGenericType();
+        //获取泛型数组中元素的类型
+        //无论从左向右有几个[]并列，getGenericComponentType仅仅脱去最右边的[]之后剩下的内容就作为这个方法的返回值。
+        System.out.println(((GenericArrayType)type).getGenericComponentType());
     }
 }
 
-class Item{
+class Item {
     private String appId;
     private String key;
     private String merchantId;
@@ -72,6 +87,10 @@ class Item{
     }
 }
 
-interface Test2{
+interface Test2 {
+
+}
+
+class STestInterface<T> {
 
 }
